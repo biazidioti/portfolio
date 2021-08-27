32735b5ec36c6b888e9fb1e811297162113b31ae
Bia Zidioti <71737694+biazidioti@users.noreply.github.com>
2021-08-27 11:49:29 -0300

    Revert "Marvel's characters"

--- my-app/src/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import search from './search';
-import cardGrid from './cardGrid';
-
-const rootReducer = combineReducers({ search, cardGrid });
-
-export default rootReducer;
--- my-app/src/services/keys.js
+++
@@ -1,2 +0,0 @@
-export const PUBLIC_KEY = '68413a57c1d1f94139c6d1e3a1c79a72'; 
-export const PRIVATE_KEY = '429a6b05dfc8dfbd131ceceeabdf83e1d40b7a55';
